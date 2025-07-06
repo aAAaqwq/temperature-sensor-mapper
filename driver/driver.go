e7d77c687800c3fbfaaf5dea5643347c3972e41d
aAAaqwq <2067089451@qq.com>
2025-07-06 09:45:06 +0800

    notes update

--- driver/driver.go
+++ driver/driver.go
@@ -22,9 +22,7 @@ func NewClient(protocol ProtocolConfig) (*CustomizedClient, error) {
 
 // InitDevice:init modbus client
 func (c *CustomizedClient) InitDevice() error {
-	// TODO: add init operation
-	// you can use c.ProtocolConfig
-	//初始化modbus客户端
+	
 	var config interface{}
 	klog.Infoln("Modbus CommunicateMode:", c.ProtocolConfig)
 	switch c.ProtocolConfig.ConfigData.CommunicateMode {
@@ -54,10 +52,10 @@ func (c *CustomizedClient) InitDevice() error {
 		klog.Errorf("Failed to create Modbus client: %v", err)
 		return err
 	}
-	// 尝试建立连接
+	
 	if err := modbusClient.Client.Connect(); err != nil {
 		klog.Errorf("Failed to connect to Modbus device: %v", err)
-		c.StopDevice() // 关闭客户端释放资源
+		c.StopDevice() 
 		return err
 	}
 	c.ModbusClient = modbusClient
@@ -70,41 +68,41 @@ func (c *CustomizedClient) GetDeviceData(visitor *VisitorConfig) (interface{}, e
 	c.deviceMutex.Lock()
 	defer c.deviceMutex.Unlock()
 
-	klog.Infof("开始从设备读取数据,Register: %s, Offset: %d, Limit: %d", visitor.Register, visitor.Offset, visitor.Limit)
+	klog.Infof("Start reading data from the device,Register: %s, Offset: %d, Limit: %d", visitor.Register, visitor.Offset, visitor.Limit)
 	data, err := c.ModbusClient.Get(visitor.Register, visitor.Offset, visitor.Limit)
 	if err != nil {
-		klog.Errorf("从设备读取数据失败: %v", err)
+		klog.Errorf("Failed to read data from the device: %v", err)
 		return nil, err
 	}
 	var value uint16
 	var warn string
 	switch visitor.Register {
 	case CoilRegister, InputRegister, DiscreteInputRegister:
-		//返回的Coil寄存器数据占用1个字节
+		//The returned Coil register data occupies 1 byte
 		value = uint16(data[0])
-		// 工作状态警告处理
+		// Working status warning processing
 		if value == 0 {
-			warn = "温度传感器已停止工作"
+			warn = "The temperature sensor has stopped working"
 		}
 	case HoldingRegister:
-		//返回的Holding寄存器数据占用2个字节
+		// The returned Holding register data occupies 2 bytes
 		value = binary.BigEndian.Uint16(data)
 		t := ZoomIn(value, visitor.Scale)
-		//温度警告处理
+		// Temperature warning handling
 		if visitor.Min != nil && t < *visitor.Min {
-			warn = fmt.Sprintf("当前温度小于最小值: %.1f < %.1f", t, *visitor.Min)
+			warn = fmt.Sprintf("The current temperature is less than the minimum value: %.1f < %.1f", t, *visitor.Min)
 		}
 		if visitor.Max != nil && t > *visitor.Max {
-			warn = fmt.Sprintf("当前温度超过最大值: %.1f > %.1f", t, *visitor.Max)
+			warn = fmt.Sprintf("The current temperature is greater than the maximum value: %.1f > %.1f", t, *visitor.Max)
 		}
 	}
-	// 根据数据类型进行转换输出
+
 	v, err := DataNormalize(value, visitor)
 	var out string
 	if warn == "" {
-		out = fmt.Sprintf("从设备读取到数据,Type:%T Value:%v", v, v)
+		out = fmt.Sprintf("The data read from the device,Type:%T Value:%v", v, v)
 	} else {
-		out = fmt.Sprintf("从设备读取到数据,Type:%T Value:%v,Warning:%s", v, v, warn)
+		out = fmt.Sprintf("The data read from the device,Type:%T Value:%v,Warning:%s", v, v, warn)
 	}
 	klog.Infof(out)
 	return out, err
@@ -123,7 +121,7 @@ func ZoomOut(data float64, scale float64) uint16 {
 	return cast.ToUint16(data / scale)
 }
 
-// DataNormalize
+// DataNormalize: data normalization
 func DataNormalize(data interface{}, visitor *VisitorConfig) (interface{}, error) {
 
 	switch visitor.DataType {
@@ -133,14 +131,14 @@ func DataNormalize(data interface{}, visitor *VisitorConfig) (interface{}, error
 		return cast.ToString(data), nil
 	case FLOAT:
 		v := cast.ToFloat32(data)
-		// 缩放
+
 		if visitor.Scale != 0 {
 			v = v * cast.ToFloat32(visitor.Scale)
 		}
 		return v, nil
 	case DOUBLE:
 		v := cast.ToFloat64(data)
-		// 缩放
+
 		if visitor.Scale != 0 {
 			v = v * visitor.Scale
 		}
@@ -150,45 +148,41 @@ func DataNormalize(data interface{}, visitor *VisitorConfig) (interface{}, error
 	case BOOLEN:
 		return cast.ToBool(data), nil
 	}
-	return nil, fmt.Errorf("无效的数据或数据类型: %v", visitor.DataType)
+	return nil, fmt.Errorf("unsupported data type: %v", visitor.DataType)
 
 }
 
-// DeviceDataWrite 外部调用DeviceMethod写入数据到设备，DeviceTwins管理层
+// DeviceDataWrite: External API call to write data to the device
 func (c *CustomizedClient) DeviceDataWrite(visitor *VisitorConfig, deviceMethodName string, propertyName string, data interface{}) error {
 	c.deviceMutex.Lock()
 	defer c.deviceMutex.Unlock()
 
-	klog.Infof("开始写入数据到设备，方法名: %s, 属性名: %s, 数据: %v, 寄存器: %s", deviceMethodName, propertyName, data, visitor.Register)
+	klog.Infof("start write data to device, method name: %s, property name: %s, data: %v, register: %s", deviceMethodName, propertyName, data, visitor.Register)
 
 	return c.SetDeviceData(data, visitor)
 }
 
-// SetDeviceData 内部写入数据到设备,Device协议层
+// SetDeviceData: Write data internally to the device
 func (c *CustomizedClient) SetDeviceData(data interface{}, visitor *VisitorConfig) error {
-	// TODO: set device's data
-	// you can use c.ProtocolConfig and visitor
-	klog.Infof("开始写入数据到设备，数据: %v,Visitor: %v", data, visitor)
+	klog.Infof("start write data to device, data: %v,visitor: %v", data, visitor)
 	if data == "" || data == nil {
-		klog.Infof("数据为空，不写入设备")
+		klog.Infof("data is empty, not write to device")
 		return nil
 	}
-	//数据类型转换
+
 	value := cast.ToUint16(ZoomOut(cast.ToFloat64(data), visitor.Scale))
-	// 写入到设备的寄存器
+
 	res, err := c.ModbusClient.Set(visitor.Register, visitor.Offset, value)
 	if err != nil {
-		klog.Errorf("写入数据到设备失败: %v", err)
+		klog.Errorf("fail to write to the device: %v", err)
 		return err
 	}
-	klog.Infof("成功写入数据到设备: %v", binary.BigEndian.Uint16((res)))
+	klog.Infof("write data to device success, value: %v", binary.BigEndian.Uint16((res)))
 	return nil
 }
 
-// StopDevice 停止设备连接
+// StopDevice : stop modbus client
 func (c *CustomizedClient) StopDevice() error {
-	// TODO: stop device
-	// you can use c.ProtocolConfig
 	err := c.ModbusClient.Client.Close()
 	if err != nil {
 		klog.Errorf("Failed to close Modbus client: %v", err)
@@ -197,14 +191,12 @@ func (c *CustomizedClient) StopDevice() error {
 	return nil
 }
 
-// GetDeviceStates 获取设备状态
+// GetDeviceStates
 func (c *CustomizedClient) GetDeviceStates() (string, error) {
-	// TODO: GetDeviceStates
-	klog.Infoln("开始检查设备状态")
+	klog.Infoln("start to check device status")
 	if err := c.ModbusClient.Client.Connect(); err != nil {
-		klog.Errorf("设备连接失败: %v", err)
+		klog.Errorf("fail to connect to device: %v", err)
 		return common.DeviceStatusDisCONN, err
 	}
-	klog.Infoln("设备状态正常")
 	return common.DeviceStatusOK, nil
 }
